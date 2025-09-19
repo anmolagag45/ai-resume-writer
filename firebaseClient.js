f3aa8b49cb89ffd00deb71a9dbaf5ce455279837
anmolagag45 <anmolagag45@email.com>
2025-09-19 22:47:31 +0530

    Initial commit - AI Resume Writer setup

---
+++ firebaseClient.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHX5Yimg4hjnQWpayRLJcWl2-p-ql5jPw",
+  authDomain: "ai-resume-writer-6f4a2.firebaseapp.com",
+  projectId: "ai-resume-writer-6f4a2",
+  storageBucket: "ai-resume-writer-6f4a2.firebasestorage.app",
+  messagingSenderId: "783487329275",
+  appId: "1:783487329275:web:f69156dc220cde53a1215e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
